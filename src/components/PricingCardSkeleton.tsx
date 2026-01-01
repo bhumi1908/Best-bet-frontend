9e44e4447a7e5b3ce77f01eee8116cca46a8da4c
bhumi1908 <bhumigangani1908@gmail.com>
2026-01-01 20:28:30 +0530

    import the file properly

--- src/components/PricingCardSkeleton.tsx
+++ src/components/PricingCardSkeleton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Skeleton } from './ui/Skeleton'
+import { Skeleton } from "@/components/ui/Skeleton";
 import { motion } from "framer-motion";
 
 const staggerContainer = {
